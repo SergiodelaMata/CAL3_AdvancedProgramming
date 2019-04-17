a1a178408f5b170d211d82d7acf53c85e1712aa3
Manuel Rodríguez <mr.manunuel@gmail.com>
2019-04-17 20:09:03 +0200

    initial commit

--- CAL3/src/cal3/Butcher.java
+++ CAL3/src/cal3/Butcher.java
@@ -5,10 +5,72 @@
  */
 package cal3;
 
+import java.util.concurrent.Semaphore;
+
+import javax.swing.JTextField;
+
 /**
  *
  * @author Sergio
  */
-public class Butcher {
+public class Butcher extends Thread{
+	private JTextField butcherText;
+	private Semaphore sem;
+    private Monitor butcherMonitor;
+	private Queue butcherQueue = new Queue(butcherText, butcherMonitor);
     
+	public Butcher(JTextField butcherText, Monitor butcherMonitor) {
+		this.butcherText = butcherText;
+		this.butcherMonitor = butcherMonitor;
+	}
+	
+	public void enter(String s)//To make the thread enter the different queues
+    {
+        butcherQueue.push(s);
+        try
+        {
+            sem.acquire();
+        }
+        catch(Exception e)
+        {}
+        butcherQueue.pop(s);
+        //if(exhibitionState)
+        //{
+        butcherQueue.push(s);
+        if(butcherMonitor.isStopThread())
+        {
+            butcherMonitor.waitResume();
+        //}
+        }
+    }
+	
+	public void buyButcher(String s)//To be during a time in the Exhibition
+    {
+        try {
+            Thread.sleep(1500+(int) (2500*Math.random()));
+            if(butcherMonitor.isStopThread())
+            {
+                butcherMonitor.waitResume();
+            }
+        }
+        catch (InterruptedException e)
+        {}
+    }
+	
+	 public void exitButcher(int v)//To make the thread go out of the Exhibition
+	    {
+	        if(true)//supermarketstate
+	        {
+	            sem.release();
+	            
+	        }
+	        if(butcherMonitor.isStopThread())
+	        {
+	            butcherMonitor.waitResume();
+	        }
+	    }
+	
+	public void run() {
+		
+	}
 }
