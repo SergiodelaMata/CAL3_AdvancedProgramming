5598d02ec3c4373cb750332e2a4de099e61477fc
SergiodelaMata <38103294+SergiodelaMata@users.noreply.github.com>
2019-04-20 19:12:43 +0200

    Situated tabulations

--- CAL3/src/cal3/Butcher.java
+++ CAL3/src/cal3/Butcher.java
@@ -13,77 +13,73 @@ import javax.swing.JTextField;
  * @author Sergio
  */
 public class Butcher extends Thread{
-	 	private int id;
-	 	private Monitor monitor;
-	    private boolean free;
-	    private String idBuyer;
-	    private JTextField text;
-	    
-	    public Butcher(int id, Monitor monitor, JTextField text)
-	    {
-	    	this.id = id;
-	        this.monitor = monitor; 
-	        this.free = true;
-	        this.idBuyer = "";
-	        this.text = text;
-	    }
-	    
-	    public void run()
-	    {
-	        while(true)
-	        {
-	            if(monitor.isStopButcher())
-	            {
-	                monitor.makeButcherWait();
-	            }
-	            buyButcher();
-	        }
-	    }
-	    
-	    
-	    public synchronized void buyButcher()
-	    {
-	        try
-	        {
-	            System.out.println(id + " " + idBuyer);
-	            monitor.setEndBuyingMeat(true);
-	            monitor.setBuyerId(idBuyer, id);
-	            monitor.activeThread();
-	        }
-	        catch(Exception e)
-	        {
-	            
-	        }
-	    }
-	    
+    private int id;
+    private Monitor monitor;
+    private boolean free;
+    private String idBuyer;
+    private JTextField text;
+    
+    public Butcher(int id, Monitor monitor, JTextField text)
+    {
+        this.id = id;
+        this.monitor = monitor;
+        this.free = true;
+        this.idBuyer = "";
+        this.text = text;
+    }
+    
+    public void run()
+    {
+        while(true)
+        {
+            if(monitor.isStopButcher())
+            {
+                monitor.makeButcherWait();
+            }
+            buyButcher();
+        }
+    }
+    
+    public synchronized void buyButcher()
+    {
+        try
+        {
+            System.out.println(id + " " + idBuyer);
+            monitor.setEndBuyingMeat(true);
+            monitor.setBuyerId(idBuyer, id);
+            monitor.activeThread();
+        }
+        catch(Exception e)
+        {
+        }
+    }
 
-	    public synchronized boolean isFree()
-	    {
-	        return free;
-	    }
-
-	    public synchronized void setFree(boolean free)
-	    {
-	        this.free = free;
-	    }
-
-	    public String getIdBuyer() {
-	        return idBuyer;
-	    }
-
-	    public synchronized void setIdBuyer(String idBuyer)
-	    {
-	        this.idBuyer = idBuyer;
-	    }
-	    
-	    public synchronized void printShow(String idBuyer)
-	    {
-	        text.setText(idBuyer);
-	    }
-	    
-	    public synchronized void printRemove()
-	    {
-	        text.setText("");
-	    }
-	    
+    public synchronized boolean isFree()
+    {
+        return free;
+    }
+    
+    public synchronized void setFree(boolean free)
+    {
+        this.free = free;
+    }
+    
+    public String getIdBuyer() {
+        return idBuyer;
+    }
+    
+    public synchronized void setIdBuyer(String idBuyer)
+    {
+        this.idBuyer = idBuyer;
+    }
+    
+    public synchronized void printShow(String idBuyer)
+    {
+        text.setText(idBuyer);
+    }
+    
+    public synchronized void printRemove()
+    {
+        text.setText("");
+    }
 }
