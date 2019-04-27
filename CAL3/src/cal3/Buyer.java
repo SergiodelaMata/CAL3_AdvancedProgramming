a96763bf26f85d278875d5aec70dab2ee3cf3f16
SergiodelaMata <38103294+SergiodelaMata@users.noreply.github.com>
2019-04-27 02:24:58 +0200

    Modified run method and included timing

--- CAL3/src/cal3/Buyer.java
+++ CAL3/src/cal3/Buyer.java
@@ -5,6 +5,8 @@
  */
 package cal3;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,23 +15,87 @@ import java.util.logging.Logger;
  * @author Sergio
  */
 public class Buyer extends Thread{
-    String idBuyer;
-    Supermarket supermarket;
-    Monitor monitor;
+    private String idBuyer;
+    private Supermarket supermarket;
+    private Monitor monitor;
+    private int maximumClientNumber;
+    private LoggerThread log;
 
-    public Buyer(String idBuyer, Monitor monitor, Supermarket supermarket) {
+    public Buyer(String idBuyer, Monitor monitor, Supermarket supermarket, int maximumClientNumber, LoggerThread log) {
         this.idBuyer = idBuyer;
         this.monitor = monitor;
         this.supermarket = supermarket;
+        this.maximumClientNumber = maximumClientNumber;
+        this.log = log;
         this.start();
     }
     
     public void run()
     {
         //int randNumber;
+        int randNumber;
+        long enterTime = 0;
+        boolean attended = false;
+        long initialTime = System.currentTimeMillis();
+        supermarket.enterOutsideQueue(idBuyer);
+        if(!monitor.isComplete())
+        {
+            enterTime = System.currentTimeMillis();
+            supermarket.enterSupermarket(idBuyer);
+            randNumber = (int) (Math.random()*3);
+            if(!monitor.isComplete())
+            {
+                switch (randNumber) {
+                    case 0:
+                        //goButcherShop(idBuyer);
+                        attended = supermarket.buyButcher(idBuyer);
+                        break;
+                    case 1:
+                        //goFishmongerShop(idBuyer);
+                        attended = supermarket.buyFishmonger(idBuyer);
+                        break;
+                    default:
+                        supermarket.goItemShelves(idBuyer);
+                        attended = true;
+                        break;
+                }
+            }
+        }
+        else
+        {
+            supermarket.goOutOutsideQueue(idBuyer);
+        }
+        if(attended)
+        {
+            supermarket.payShopping(idBuyer);
+        }    
+        supermarket.exitSupermarket(idBuyer);
         
-        
-        supermarket.shopping(idBuyer);
+        //supermarket.shopping(idBuyer);
+        long finalTime = System.currentTimeMillis();
+        if(enterTime != 0)
+        {
+            supermarket.addEnterAndLeaveTime(finalTime - enterTime);
+        }
+        supermarket.addWaitOutsideQueueAndLeaveTime(finalTime - initialTime);
+        //System.out.println(supermarket.getCounterBuyerExit().getCounter() + " " + maximumClientNumber);
+        if(supermarket.getCounterBuyerExit().getCounter() >= maximumClientNumber)
+        {
+            log.log("");
+            log.log("Butcher shop average service time: " + supermarket.counterAverageServiceTime(supermarket.getTimeServiceButcherList()) + "ms");
+            log.log("Fish shop average service time: " + supermarket.counterAverageServiceTime(supermarket.getTimeServiceFishmongerList()) + "ms");
+            log.log("Accumulation times at the butcher shop: " + supermarket.getAccumulationTimesButcher().getCounter());
+            log.log("Accumulation times at the fish shop: " + supermarket.getAccumulationTimesFishmonger().getCounter());
+            log.log("Total number of people who have entered the supermarket: " + supermarket.getCounterBuyerEnter().getCounter());
+            log.log("Total number of people who have left the supermarket: " + supermarket.getCounterBuyerExit().getCounter());
+            log.log("Average time from a buyer since he/she arrives at the outside queue until he/she leaves the supermarket: " + supermarket.counterAverageServiceTime(supermarket.getTimeWaitOutsideQueueAndLeaveList()) + "ms");
+            log.log("Average time from a buyer since he/she enters the supermarket until he/she leaves the supermarket: " + supermarket.counterAverageServiceTime(supermarket.getTimeEnterAndLeaveList()) + "ms");
+            /*while(log.getCounter() < log.length())
+            {
+                System.out.println(log.getCounter() + " de " + log.length());
+                //System.out.print(log.getData(log.getCounter()));
+            }*/
+        }
         
         //supermarket.enterSupermarket(idBuyer);
         
@@ -63,9 +129,5 @@ public class Buyer extends Thread{
             Logger.getLogger(Buyer.class.getName()).log(Level.SEVERE, null, ex);
         }*/
         /*supermarket.exitSupermarket(idBuyer);*/
-    }
-
-    private void Sleep(double d) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
