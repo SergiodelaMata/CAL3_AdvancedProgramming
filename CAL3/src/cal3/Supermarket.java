b458bbfa0229ef3afba048c1558cfce7de4f6b82
SergiodelaMata <38103294+SergiodelaMata@users.noreply.github.com>
2019-05-04 16:55:58 +0200

    Include a method to get CheckAreaQueue

--- CAL3/src/cal3/Supermarket.java
+++ CAL3/src/cal3/Supermarket.java
@@ -17,7 +17,6 @@ import javax.swing.JTextField;
  * @author Sergio, Manuel
  */
 public class Supermarket {
-    //private int maximumClientNumber;
     private JTextField numberPeopleShelves;
     private Monitor monitor;
     private Semaphore semOutsideQueue, semButcherQueue, semFishmongerQueue, semCheckAreaQueue;
@@ -64,45 +63,7 @@ public class Supermarket {
         this.timeWaitingFishmongerList = new ArrayList<>();
         this.timeEnterAndLeaveList = new ArrayList<>();
         this.timeWaitOutsideQueueAndLeaveList = new ArrayList<>();
-        //this.maximumClientNumber = maximumClientNumber;
-        
     }
-    
-    /**public void shopping(String idBuyer)
-    {
-        int randNumber;
-        boolean attended = false;
-        enterSupermarket(idBuyer);
-        lock.lock();
-        try {
-            randNumber = (int) (Math.random()*3);
-        } finally {
-            lock.unlock();
-        }
-        if(!monitor.isComplete())
-        {
-            switch (randNumber) {
-                case 0:
-                    //goButcherShop(idBuyer);
-                    attended = buyButcher(idBuyer);
-                    break;
-                case 1:
-                    //goFishmongerShop(idBuyer);
-                    attended = buyFishmonger(idBuyer);
-                    break;
-                default:
-                    goItemShelves(idBuyer);
-                    attended = true;
-                    break;
-            }
-        }
-        if(attended)
-        {
-            payShopping(idBuyer);
-        }    
-        exitSupermarket(idBuyer);
-        
-    }*/
     
     public void enterOutsideQueue(String idBuyer)
     {
@@ -113,32 +74,19 @@ public class Supermarket {
     
     public void enterSupermarket(String idBuyer)
     {
-        //outsideQueue.push(idBuyer);
         try
         {
-            
-            //System.out.println("hola 1");
-            //monitor.waitIfDifferentIdBuyerEnterSupermarket(idBuyer, outsideQueue, insideBuyerCounter);
-            //if(!monitor.isComplete())
-            //{
-                semOutsideQueue.acquire();
-                insideBuyerCounter.setCounter(insideBuyerCounter.getCounter()+1);
-                counterBuyerEnter.setCounter(counterBuyerEnter.getCounter()+1);
-                //System.out.println("hola 2 " + idBuyer);
-                if(monitor.isStopThread())
-                {
-                    monitor.waitResume();
-                }
-                outsideQueue.pop(idBuyer);
-                System.out.println(idBuyer + " enters the supermarket");
-                log.log(idBuyer + " enters the supermarket");
-                monitor.activeThread();
-                //System.out.println("hola 3 " + idBuyer);
-            //}
-            //else
-            //{
-            //    outsideQueue.pop(idBuyer);
-            //}
+            semOutsideQueue.acquire();
+            insideBuyerCounter.setCounter(insideBuyerCounter.getCounter()+1);
+            counterBuyerEnter.setCounter(counterBuyerEnter.getCounter()+1);
+            if(monitor.isStopThread())
+            {
+                monitor.waitResume();
+            }
+            outsideQueue.pop(idBuyer);
+            System.out.println(idBuyer + " enters the supermarket");
+            log.log(idBuyer + " enters the supermarket");
+            monitor.activeThread();
         }
         catch(Exception e)
         {}
@@ -166,13 +114,6 @@ public class Supermarket {
         }
         counterBuyerExit.setCounter(counterBuyerExit.getCounter()+1);
         monitor.activeThread();
-        /**if(counterBuyerExit.getCounter() >= maximumClientNumber)
-        {
-            log.log("Butcher shop average service time: " + counterServiceTime(timeServiceButcherList) + "ms");
-            log.log("Fish shop average service time: " + counterServiceTime(timeServiceFishmongerList) + "ms");
-            log.log("Total number of people who have entered the supermarket: " + counterBuyerEnter.getCounter());
-            log.log("Total number of people who have left the supermarket: " + counterBuyerExit.getCounter());
-        }*/
     }
     
     public void payShopping(String idBuyer)
@@ -181,7 +122,6 @@ public class Supermarket {
         log.log(idBuyer + " waits at the checkout area");
         try
         {
-            //sleep(2000);
             monitor.waitIfDifferentIdBuyerEnterCashier(idBuyer, checkArea.getPayQueue(), cashierAttendingCounter);
             semCheckAreaQueue.acquire();
             if(monitor.isStopThread())
@@ -271,18 +211,7 @@ public class Supermarket {
         //System.out.println(idBuyer + " finished buying");
         semCheckAreaQueue.release();
     }
-    
-    /**public void goButcherShop(String idBuyer)// Enter butcher´s
-	{
-		butcherQueue.push(idBuyer);
-                log.log(idBuyer + " waits at the butcher shop");
-		if (monitor.isStopThread()) {
-			monitor.waitResume();
-		}
-		// System.out.println(idBuyer + " Butcher");
-		butcherQueue.remove(idBuyer);
-	}*/
-    
+        
     public boolean buyButcher(String idBuyer) {
         boolean attended = false;
         long randNumber;
@@ -582,7 +511,9 @@ public class Supermarket {
     public Counter getAccumulationTimesFishmonger() {
         return accumulationTimesFishmonger;
     }
-    
-    
+
+    public CheckArea getCheckArea() {
+        return checkArea;
+    }
     
 }
